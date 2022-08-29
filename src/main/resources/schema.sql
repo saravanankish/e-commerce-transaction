8ff3374e13a28a9dc54a14743eea5c5ee74a1969
Saravanan Kishorekumar <saravanan.kishorekumar@bounteous.com>
2022-08-29 12:39:54 +0530

    Added schema file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `transactions` (
+  `transaction_id` bigint NOT NULL AUTO_INCREMENT,
+  `amount` float NOT NULL,
+  `order_id` bigint DEFAULT NULL,
+  `received` bit(1) NOT NULL,
+  `success` bit(1) NOT NULL,
+  `transaction_date` datetime(6) DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`)
+);
