46eb95a54386233b22487f7d8002cab125303264
Saravanan Kishorekumar <saravanan.kishorekumar@bounteous.com>
2022-08-30 17:37:23 +0530

    Change in schema file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `transactions` (
+CREATE TABLE IF NOT EXISTS `transactions` (
   `transaction_id` bigint NOT NULL AUTO_INCREMENT,
   `amount` float NOT NULL,
   `order_id` bigint DEFAULT NULL,
